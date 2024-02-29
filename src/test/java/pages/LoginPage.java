b823af92f5f59460ad711d0972b3e4dce07aeaa4
diamond2602 <39782754+diamond2602@users.noreply.github.com>
2024-02-29 09:56:50 -0800

    postive login completed,

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,4 +1,29 @@
 package pages;
 
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(id = "email")
+    WebElement email;
+
+    @FindBy(id = "password")
+    WebElement password;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    WebElement loginButton;
+
+    public void login(String email,String password){
+        this.email.sendKeys(email);
+        this.password.sendKeys(password);
+        loginButton.click();
+    }
+
 }
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,4 @@
+Feature: Testing the Login Functionality of smartbear Website
+
+  Scenario: Happy Path Login Functionality(Positive) for Techtorial CRM
+    Given User login to to page with valid credentials
